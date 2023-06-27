09b5fee8bbd109784eaea20040821e93b9b93197
zamami <s1413115@gmail.com>
2023-06-27 11:05:38 +0900

    deviseカラムの追加、 devise:viewの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_25_055358) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_25_083839) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,6 +28,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_055358) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "zip"
+    t.string "address"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
