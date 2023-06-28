01c6d368ec3f66bf02fcc2a787084a85c7a91a7d
zamami <s1413115@gmail.com>
2023-06-29 07:46:35 +0900

    ログアウト後の遷移先エラーを解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    books_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(_)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
