4a558889c150507a80292ea6b5260170cb2b3dd3
zamami <s1413115@gmail.com>
2023-07-03 08:12:01 +0900

    deviseのモデルにカラムを追加、変種画面にラベルとフィールドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path # ログイン後に遷移するpathを設定
   end
 
   def after_sign_out_path_for(_)
     new_user_session_path # ログアウト後に遷移するpathを設定
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name zip address profile])
   end
 end
