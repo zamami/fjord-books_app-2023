8ad7f7b853f7b55f52ac1e85178315eede22974f
zamami <s1413115@gmail.com>
2023-07-03 11:02:35 +0900

    ユーザー一覧、詳細画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  resources :users, :only => [:index, :show]
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
