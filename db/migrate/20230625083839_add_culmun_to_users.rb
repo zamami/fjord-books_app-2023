1caf962c50b8e4c60d517d1b5b6020db07eea219
zamami <s1413115@gmail.com>
2023-07-15 17:58:16 +0900

    ルーティングの修正、shemaの修正、formの修正

--- db/migrate/20230625083839_add_culmun_to_users.rb
+++ db/migrate/20230625083839_add_culmun_to_users.rb
@@ -1,7 +1,7 @@
 class AddCulmunToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :zip, :integer
+    add_column :users, :zip, :string
     add_column :users, :address, :string
     add_column :users, :profile, :text
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_031331) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_25_083839) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,8 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_031331) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", null: false
-    t.integer "zip"
+    t.string "name"
+    t.string "zip"
     t.string "address"
     t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
