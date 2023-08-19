ef504cc6d5fc857a0dffcacaf92f1dda80885373
zamami <s1413115@gmail.com>
2023-08-19 19:58:05 +0900

    rubocopに指摘された箇所を修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
   belongs_to :commentable, polymorphic: true
-  belongs_to :user, foreign_key: :user_id
+  belongs_to :user
 end
