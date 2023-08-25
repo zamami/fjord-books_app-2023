d8c1cb09a574e1d7c57ed9c0cd694603e209b0d8
zamami <s1413115@gmail.com>
2023-08-25 14:45:20 +0900

    メソッド名変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,8 +7,7 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  def self_created_report?(current_user)
+  def created_by?(current_user)
     user == current_user
   end
 end
-
