c684609619fffb26b85a1c0d1a91141df6a4dbab
zamami <s1413115@gmail.com>
2023-09-02 21:27:47 +0900

    reportモデルの自己結合およびリレーション設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,26 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :active_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioning_report_id',
+           inverse_of: :mentioning_report,
+           dependent: :destroy
+
+  has_many :mentioning_reports,
+           through: :active_mentions,
+           source: :mentioned_report
+
+  has_many :passive_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioned_report_id',
+           inverse_of: :mentioned_report,
+           dependent: :destroy
+
+  has_many :mentioned_reports,
+           through: :passive_mentions,
+           source: :mentioning_report
+
   validates :title, presence: true
   validates :content, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_032711) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "mentions", force: :cascade do |t|
+    t.integer "mentioning_report_id", null: false
+    t.integer "mentioned_report_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mentioned_report_id"], name: "index_mentions_on_mentioned_report_id"
+    t.index ["mentioning_report_id", "mentioned_report_id"], name: "index_mentions_on_mentioning_report_id_and_mentioned_report_id", unique: true
+    t.index ["mentioning_report_id"], name: "index_mentions_on_mentioning_report_id"
+  end
+
   create_table "reports", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
@@ -87,5 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "mentions", "reports", column: "mentioned_report_id"
+  add_foreign_key "mentions", "reports", column: "mentioning_report_id"
   add_foreign_key "reports", "users"
 end
