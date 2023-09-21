04b02bb4e851e148e4a524e0dc6c6882ae7ed6b0
zamami <s1413115@gmail.com>
2023-09-22 08:53:59 +0900

    フィクスチャのデータを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,22 +4,22 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#レポートを書いたユーザーかどうか？' do
-    alice = users(:Alice)
-    bob = users(:Bob)
-    alice_report = reports(:Alice_report)
+    alice = users(:alice)
+    bob = users(:bob)
+    alice_report = reports(:alice_report)
     assert alice_report.editable?(alice)
     assert_not alice_report.editable?(bob)
   end
 
   test '#レポートが作られた日付が正しいか確認' do
-    alice_report = reports(:Alice_report)
+    alice_report = reports(:alice_report)
     today = Time.zone.today
     assert_equal today, alice_report.created_on
   end
 
   test '#メンションしているか、されているか？' do
-    alice_report = reports(:Alice_report)
-    bob_report = reports(:Bob_report)
+    alice_report = reports(:alice_report)
+    bob_report = reports(:bob_report)
     assert_not_includes alice_report.mentioning_reports, bob_report
     assert_not_includes bob_report.mentioned_reports, alice_report
     alice_report.mentioning_reports.push bob_report
