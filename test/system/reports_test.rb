d65e9bb91a34c25202026f697d7e52bb58c637fd
zamami <s1413115@gmail.com>
2023-09-22 08:15:50 +0900

    reportのシステムテストを作成

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -12,41 +12,37 @@ class ReportsTest < ApplicationSystemTestCase
     assert_text 'ログインしました。' # CapybaraにTurbolinksの完了を待たせる
   end
 
-  test 'visiting the index' do
+  test 'ログイン後、日報の一覧ページが表示できるか' do
     visit reports_url
     assert_selector 'h1', text: '日報の一覧'
   end
 
-  test 'should create report' do
-    visit reports_url
-    click_on 'New report'
-
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
-
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+  test 'メールアドレスとパスワードでログインして日報を書く' do
+    click_on '日報'
+    # assert_selector 'h1', text: '日報'
+    click_on '日報の新規作成'
+    fill_in 'タイトル', with: 'はじめての日報'
+    fill_in '内容', with: '今日はminiテストについて学びました！'
+    click_on '登録する'
+    assert_text '日報が作成されました。'
+    assert_text 'はじめての日報'
+    assert_text '今日はminiテストについて学びました！'
   end
 
-  test 'should update Report' do
+  test '日報の更新ができているか' do
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
-
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
-
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    click_on 'この日報を編集', match: :first
+    fill_in 'タイトル', with: '日報を更新します！'
+    fill_in '内容', with: '日報の更新完了！'
+    click_on '更新する'
+    assert_text '日報が更新されました。'
+    assert_text '日報を更新します！'
+    assert_text '日報の更新完了！'
   end
 
-  test 'should destroy Report' do
+  test '日報が削除できているか' do
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
-
-    assert_text 'Report was successfully destroyed'
+    click_on 'この日報を削除', match: :first
+    assert_text '日報が削除されました。'
   end
 end
